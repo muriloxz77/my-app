6b57cc53fadd3ce144bdace25e154997de65191c
muriloxz77 <muriloaugusto9374@gmail.com>
2024-02-22 16:19:18 -0300

    .

---
+++ senai-pags/screens/cadastro.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+function Cadastro(){
+    return(
+        <View>
+            <Text>Cadastre-se</Text>
+        </View>
+    )
+}
+export default Cadastro
